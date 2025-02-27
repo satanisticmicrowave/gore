cb04c0f71db7c7b6b46e7b1f81294ab5a9b0a8a4
Satanistic microwave <satanisticmicrowave@ya.ru>
2025-02-27 14:17:40 +0300

    ADD: file separator

---
+++ gore/core/sys/file_separator.hpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2025-02-27 13:32:43 satanisticmicrowave. All rights reserved.
+ *
+ * File separator definition
+ *
+ * Author: satanisticmicrowave <satanisticmicrowave@ya.ru>
+ * Date: 2025-02-27 13:32:43
+ * File: gore/core/sys/file_separator.hpp
+ *
+ * Made with love :3
+ */
+
+#ifndef SYS_FILE_SEPARATOR_H
+#define SYS_FILE_SEPARATOR_H
+
+#if defined(_WIN32)
+#define FSPEPARATOR "\\"
+#else
+#define FSEPARATOR "/"
+#endif
+
+#endif // !SYS_FILE_SEPARATOR_H
