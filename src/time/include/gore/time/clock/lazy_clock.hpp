5ec80784f2465d798e68e1d2375918482e151901
Satanistic Microwave <satanisticmicrowave@ya.ru>
2025-06-22 02:55:36 +0300

    MOD: CMakeLists.txt
    -- Added windows compabitility

--- src/time/include/gore/time/clock/lazy_clock.hpp
+++ src/time/include/gore/time/clock/lazy_clock.hpp
@@ -12,28 +12,9 @@
 #ifndef GORE_LAZY_CLOCK_HPP_522195
 #define GORE_LAZY_CLOCK_HPP_522195
 
-#include <atomic>
-#include <chrono>
-
 namespace gore::time {
 
-class lazy_clock final {
-public:
-  using ret_time = std::chrono::milliseconds;
-
-  explicit lazy_clock() noexcept;
-
-  [[nodiscard("lazy_clock::get_last_time() - returned value is ignored")]]
-  ret_time get_last_time() const noexcept;
-
-  [[nodiscard("lazy_clock::get_current_time() - returned value is ignored")]]
-  ret_time get_current_time() noexcept;
-
-private:
-  std::atomic<ret_time> time;
-  const std::chrono::steady_clock::time_point time_created =
-      std::chrono::steady_clock::now();
-};
+class lazy_clock final {};
 
 } // namespace gore::time
 
--- src/time/src/clock/lazy_clock.cpp
+++ src/time/src/clock/lazy_clock.cpp
@@ -9,19 +9,6 @@
 
 #include "../../include/gore/time/clock/lazy_clock.hpp"
 
-namespace gore::time {
+#define DEFAULT_REFRESH_INTERVAL 10
 
-lazy_clock::lazy_clock() noexcept { this->time.store(ret_time::zero()); }
-
-std::chrono::milliseconds lazy_clock::get_last_time() const noexcept {
-  return this->time.load();
-}
-
-std::chrono::milliseconds lazy_clock::get_current_time() noexcept {
-  auto delta = std::chrono::steady_clock::now() - this->time_created;
-  this->time.store(std::chrono::duration_cast<ret_time>(delta));
-
-  return this->get_last_time();
-}
-
-} // namespace gore::time
+namespace gore::time {} // namespace gore::time
